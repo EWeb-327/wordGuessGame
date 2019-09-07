76605c697508710a1d9c2094ebf2858c9445fb96
Emma Weber <emma.weber327@gmail.com>
2019-09-07 11:32:02 -0600

    created a sample.js to house JS psuedocode and build ideas. Input JS to game.js and linked with html. User guess built, Computer word selection and blanks built, guess count movement built.

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -1,35 +1,45 @@
-var zooAnimals = ["Giraffe", "Lion", "Tiger", "Hippo", "Elephant", "Zebra", "Polar Bear", "Okapi", "Camel", "Penguin", "Otter", "Peacock", "Rhino", "Monkey", "Panda", "Sloth", "Gorilla", "Koala", "Kangaroo", "Wolf", "Tortoise", "Lemur", "Cheetah", "Alligator", "Capybara", "Meerkat"];
+//var to hold array of computer choices
+var zooAnimals = ["giraffe", "lion", "tiger", "hippo", "elephant", "zebra", "snake", "okapi", "camel", "penguin", "otter", "peacock", "rhino", "monkey", "panda", "sloth", "gorilla", "koala", "kangaroo", "wolf", "tortoise", "lemur", "cheetah", "alligator", "capybara", "meerkat"];
+//var for computerRandomWord
 var computerWord = zooAnimals[Math.floor(Math.random() * zooAnimals.length)];
-
+//var to split computerWord
+var wordLetters = computerWord.split("")
+//var for guesses, wins
 var wins = 0;
 var guesses = 15;
-
+//vars for quick references
 var directionsText = document.getElementById("directions");
-var userChoiceText = document.getElementById("user-letter");
+var userWrongGuessText = document.getElementById("letter-guessed");
+var userRightGuessText = document.getElementById("current-word");
 var computerChoiceText = document.getElementById("random-word");
-var guessCountText = document.getElementById("guess-count");
-var winsText = document.getElementById("wins");
+var guessesText = document.getElementById("guess-count")
+var winsText = document.getElementById("wins")
 
-
+//key pressed function
 document.onkeyup = function(event){
-    var userGuess = event.key;
-
-    if (guesses === 0 || guesses === 15) {
-        console.log(computerWord)
-        // console.log(word.length)
+//var for userGuess
+    var userGuess = event.key; //the key the user pressed
+    //wordGenerator function
+    function wordGenerator(word) {
+        var length = word.length
+        var emptyString = ""
+        console.log(length)
+        for (var i = 0; i < length; i++) {
+            emptyString = emptyString + "_ "
+        }
+        computerChoiceText.textContent = emptyString
     }
-
-
+    //what happens when space is pressed
+    console.log(computerWord)
+    directionsText.textContent = ""
+    wordGenerator(computerWord)
+    //what happens with each userGuess
     if (computerWord.includes(userGuess)) {
-       console.log("The word includes: " + userGuess);
-       guesses --;
+            userRightGuessText.textContent = userGuess
+            guesses --;
     } else {
-        console.log("Letters Guessed: \n" + userGuess)
+        userWrongGuessText.textContent = userGuess 
         guesses --;
     }
-    directionsText.textContent = "";
-    winsText.textContent = "Wins: " + wins
-    guessCountText.textContent = "Guesses Left: " + guesses
-    console.log(computerWord)
+ 
 }
-    
